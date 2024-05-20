434e0d0eb1f27273de8c8fd47158968a5b6e73bc
Josh Graber <josh.graber@therapynotes.com>
2024-05-20 17:02:34 -0400

    Dummy application illustrating cancellation tokens

---
+++ Extensions.cs
@@ -0,0 +1,17 @@
+﻿namespace CancellationTokenTest;
+
+public static class Extensions
+{
+	public static async Task Times(this int count, Func<int, Task> action, CancellationToken token)
+	{
+		for (var i = 0; i < count; i++)
+		{
+			if (token.IsCancellationRequested)
+			{
+				return;
+			}
+				
+			await action(i);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+using CancellationTokenTest;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+const int repetitions = 20;
+
+app.MapGet("/long-request", async (CancellationToken token) =>
+	{
+		token.ThrowIfCancellationRequested();
+		
+		try
+		{
+			await repetitions.Times(async (i) =>
+			{
+				Console.WriteLine($"{i}: Long request running...");
+				await Task.Delay(1000, token);
+			}, token);
+			
+			Console.WriteLine("Long request complete.");
+			
+			return new Random().Next(0, 100);
+		}
+		catch (OperationCanceledException e)
+		{
+			Console.WriteLine("Long request cancelled.");
+			throw;
+		}
+	})
+	.WithName("LongRequest")
+	.WithOpenApi();
+
+app.Run();
